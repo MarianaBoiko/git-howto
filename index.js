b97b0fe2ebdfaff7a6f26c7d342d591f5642686f
mariana.boiko <mariana.boiko@polygons.at>
2025-02-03 23:39:24 +0100

    Deleted comments

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function sayHello(name, surname, anotherParam) {
   console.log(`Hello ${name} ${surname} ${anotherParam}`);
 }
-//Some additional comment
+
 sayHello("Mariana", "Boiko");
-//Some additional comment
